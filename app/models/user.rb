55a4fd5b134399633122d33e7b6007202fc92a47
DmitriyMalayev <dmitriy.malayev@gmail.com>
2020-08-27 15:00:22 -0400

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
+#Our only model is User 
+#This validates some of the attributes of our user so no one can login without inputting their name, email and password. 
+
 class User < ActiveRecord::Base
   validates_presence_of :name, :email, :password
 end

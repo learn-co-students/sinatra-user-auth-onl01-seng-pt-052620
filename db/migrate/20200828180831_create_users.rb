438544203329d61a0670767d586027b7aee1647a
bebofofum <bri.boehm@gmail.com>
2020-08-28 15:13:45 -0400

    Done.

---
+++ db/migrate/20200828180831_create_users.rb
@@ -0,0 +1,11 @@
+class CreateUsers < ActiveRecord::Migration
+  
+  def change
+    create_table :users do |c|
+      c.string :name
+      c.string :email
+      c.string :password
+    end
+
+  end
+end
